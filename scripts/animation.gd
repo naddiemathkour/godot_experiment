f1f5009f946a17ac6eacbd78736d979fd2438a00
naddiemathkour <naddiemathkour@gmail.com>
2023-03-30 14:22:04 -0400

    animation_unlocked state added

--- scripts/animation.gd
+++ scripts/animation.gd
@@ -1,28 +1,28 @@
 extends AnimatedSprite2D
 
-@onready var animation_lock = false
+@onready var animation_unlocked = true
 
 func get_input_axis():
 	var curr_vector = Input.get_vector("move_down", "move_up", "move_left", "move_right")
 
-	if (animation != "3_strike_combo"):
+	if (animation_unlocked):
 		if (curr_vector == Vector2(0,0)):
 			play("idle")
 		else:
 			play("run")
-	if (Input.is_action_just_pressed("move_left")):
-		flip_h = true
-	elif (Input.is_action_just_pressed("move_right")):
-		flip_h = false
+		if (Input.is_action_just_pressed("move_left")):
+			flip_h = true
+		elif (Input.is_action_just_pressed("move_right")):
+			flip_h = false
 	print(animation)
 
 	if(Input.is_action_pressed("attack")):
 		play("3_strike_combo")
-		animation_lock = true
+		animation_unlocked = false
 
 func _physics_process(delta):
 	get_input_axis()
 
 
 func _on_animation_finished():
-	stop()
+	animation_unlocked = true
