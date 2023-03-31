e9ce77c1eb955abe7818279cf4570996f1f336ad
naddiemathkour <naddiemathkour@gmail.com>
2023-03-30 23:13:42 -0400

    skeleton movement enhancements

--- scripts/basic_enemy.gd
+++ scripts/basic_enemy.gd
@@ -4,17 +4,24 @@ extends CharacterBody2D
 @onready var movement_lock = false
 @onready var player = get_parent().get_node("player")
 @onready var this_enemy = self
+@onready var walk_around = false
 
 func get_input():
 	var curr_vector = Vector2.ZERO
-	if (player.position.x > this_enemy.position.x):
+	if (!walk_around):
+		if (player.position.x > this_enemy.position.x):
+			curr_vector += Vector2(.1,0)
+		if (player.position.x < this_enemy.position.x):
+			curr_vector += Vector2(-.1,0)
+		if (player.position.y > this_enemy.position.y):
+			curr_vector += Vector2(0,.1)
+		if (player.position.y < this_enemy.position.y):
+			curr_vector += Vector2(0,-.1)
+	if (abs(player.position.x - this_enemy.position.x) < 7.8):
+		walk_around = true
 		curr_vector += Vector2(.25,0)
-	if (player.position.x < this_enemy.position.x):
-		curr_vector += Vector2(-.25,0)
-	if (player.position.y > this_enemy.position.y):
-		curr_vector += Vector2(0,.25)
-	if (player.position.y < this_enemy.position.y):
-		curr_vector += Vector2(0,-.25)
+	else:
+		walk_around = false
 	
 	#if ($basic_enemy_model.animation != "basic_attack"):
 	velocity = curr_vector * speed
@@ -22,5 +29,7 @@ func get_input():
 #		velocity = Vector2.ZERO
 
 func _physics_process(delta):
+	if (true):
+		pass
 	get_input()
 	move_and_slide()
--- scripts/basic_enemy_amination.gd
+++ scripts/basic_enemy_amination.gd
@@ -4,34 +4,26 @@ extends AnimatedSprite2D
 @onready var player = get_parent().get_parent().get_node("player")
 @onready var this_enemy = get_parent()
 
-func get_input_axis():
+func run():
 	var curr_vector:Vector2
 
 	if (animation_unlocked):
-		if (curr_vector == Vector2(0,0)):
+		if (this_enemy.position == player.position):
 			play("idle")
 		else:
 			play("run")
-		if (Input.is_action_just_pressed("move_left")):
+		if (player.position.x < this_enemy.position.x):
 			flip_h = true
-		elif (Input.is_action_just_pressed("move_right")):
+		elif (player.position.x > this_enemy.position.x):
 			flip_h = false
-	print(player.position.x)
-	print(get_parent().position)
-	
-	if(Input.is_action_pressed("attack")):
+	print(abs(player.position.y - this_enemy.position.y))
+	if(abs(player.position.x - this_enemy.position.x) < 25 && abs(player.position.y - this_enemy.position.y) < 7.5):
 		play("basic_attack")
-		animation_unlocked = false
-	elif(Input.is_action_just_released("attack")):
-		stop();
-		animation_unlocked = true
-
-	if(Input.is_action_pressed("special_attack")):
-		play("special_attack")
 		animation_unlocked = false
 
 func _physics_process(delta):
-	get_input_axis()
+	run()
+
 
 func _on_animation_finished():
 	animation_unlocked = true
